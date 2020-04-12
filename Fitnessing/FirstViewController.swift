974c242b7aed6a8ae790c37bdf42b487dd3ccdfd
kumikorandle <kcrandle98@hotmail.com>
2020-04-12 16:09:57 -0400

    Initial Commit

---
+++ Fitnessing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Fitnessing
+//
+//  Created by Kumiko Randle on 2020-04-12.
+//  Copyright © 2020 Kumiko Randle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fitnessing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Fitnessing
+//
+//  Created by Kumiko Randle on 2020-04-12.
+//  Copyright © 2020 Kumiko Randle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
